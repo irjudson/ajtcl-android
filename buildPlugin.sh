0092264da2484ef8d900953d6a5473deae5de935
Ivan R. Judson <irjudson@gmail.com>
2015-02-01 14:04:57 -0800

    More fixes

--- buildPlugin.sh
+++ buildPlugin.sh
@@ -19,18 +19,19 @@ else
 	AJTCL_ROOT="$2"
 fi
 
-if [ "$AJTCL_ROOT" == "" ]; then
-	echo "Please set your AllJoyn Thin Client Library Root directory (AJTCL_ROOT) to the location of your AJTCL code and run the build process with the LANG=java option to generate the java bindings."
+
+if [ "$3x" == "x" ]; then
+	echo "NDK_ROOT not set. Exiting..."
 	exit 1
+else
+	NDK_ROOT="$3"
 fi
 
-# Copy the bits and make the jar
+# Copy the generated files
 cp $AJTCL_ROOT/swig/alljoyn_java_wrap.c $PLUGINDIR/jni/alljoyn.c
 cp $AJTCL_ROOT/swig/java/* $PLUGINDIR/src/alljoyn/
-javac $PLUGINDIR/src/alljoyn/*.java
-jar -cvf $PLUGINDIR/alljoyn.jar -C $PLUGINDIR/src alljoyn
 
 # Build the native assets
 pushd $PLUGINDIR
-ndk-build
+$NDK_ROOT/ndk-build
 popd
